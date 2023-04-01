1a1959b686da302bc5e3c51cc644beee602e9a75
MinsuKim <minsu020302@gmail.com>
2023-04-02 02:22:35 +0900

    Design : img크기 조정

--- src/pages/Actives.jsx
+++ src/pages/Actives.jsx
@@ -5,12 +5,12 @@ import Carousel from '../components/carousel/Carousel';
 
 export default function Actives() {
   return (
-    <section className="MJU-Developers" id='actives'>
-      <img src={bgImg1} alt="background image" />
-      <article className="bg-custom text-slate-800">
+    <section className="MJU-Developers" id="actives">
+      <img src={bgImg1} alt="background image" className="w-full" />
+      <article className="bg-custom text-slate-800 py-6">
         <Carousel />
       </article>
-      <img src={bgImg2} alt="background image" />
+      <img src={bgImg2} alt="background image" className="w-full" />
     </section>
   );
 }
